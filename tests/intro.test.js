7833b3f0ed9f34d1b10389746caabeee7948f844
IanD1013 <ian.tung1013@gmail.com>
2024-12-11 07:44:17 +1300

    Exercise - testing fizzBuzz

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { max } from "../src/intro";
+import { max, fizzBuzz } from "../src/intro";
 
 describe("max", () => {
   it("should return the first argument if it is greater", () => {
@@ -21,5 +21,23 @@ describe("max", () => {
 
   it("should return the first argument if arguments are equal", () => {
     expect(max(1, 1)).toBe(1);
+  });
+});
+
+describe("fizzBuzz", () => {
+  it("should return FizzBuzz if arg is divisible by 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+
+  it("should return Fizz if arg is only divisible by 3", () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
+  });
+
+  it("should return Buzz if arg is only divisible by 5", () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
+  });
+
+  it("should return the arg as a string if not divisible by 3 or 5", () => {
+    expect(fizzBuzz(1)).toBe("1");
   });
 });
