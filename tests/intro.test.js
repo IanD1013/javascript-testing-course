e7f91963451dc632630400125eeaa5c9e474f1a7
IanD1013 <ian.tung1013@gmail.com>
2024-12-16 18:14:52 +1300

    Test-driven development

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { max, fizzBuzz } from "../src/intro";
+import { max, fizzBuzz, calculateAverage } from "../src/intro";
 
 describe("max", () => {
   it("should return the first argument if it is greater", () => {
@@ -39,5 +39,23 @@ describe("fizzBuzz", () => {
 
   it("should return the arg as a string if not divisible by 3 or 5", () => {
     expect(fizzBuzz(1)).toBe("1");
+  });
+});
+
+describe("calculateAverage", () => {
+  it("should return NaN if given an empty array", () => {
+    expect(calculateAverage([])).toBe(NaN);
+  });
+
+  it("should calculate the average of an array with a single element", () => {
+    expect(calculateAverage([1])).toBe(1);
+  });
+
+  it("should calculate the average of an array with 2 elements", () => {
+    expect(calculateAverage([1, 2])).toBe(1.5);
+  });
+
+  it("should calculate the average of an array with 3 elements", () => {
+    expect(calculateAverage([1, 2, 3])).toBe(2);
   });
 });
